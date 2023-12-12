763ed8437553de867c9b24193fb1e96d602acf1b
Daniil <120030682+Danikaster@users.noreply.github.com>
2023-12-12 14:56:34 +0300

    Update main.cpp

--- raw/CurseWorkQt/main.cpp
+++ raw/CurseWorkQt/main.cpp
@@ -14,18 +14,10 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
-//    QApplication a(argc, argv);
+    QApplication a(argc, argv);
 
-//    gamewidget w;
-//    w.show();
-    Field field;
-    field.gen->generate(field.field, 30);
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            std::cout<<field.field[i][j].value<<" ";
-        }
-        std::cout<<std::endl;
-    }
-
-    //return a.exec();
+    gamewidget w;
+    w.show();
+    
+    return a.exec();
 }
